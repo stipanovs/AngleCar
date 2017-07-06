331bf5b1c5c154379175c43d3ba31ae112f8b455
stipanovs <sergiu.stipanov@amdaris.com>
2017-07-06 13:08:54 +0300

    first commit

--- AngleOverloadOperatorSortCarSpeed/Program.cs
+++ AngleOverloadOperatorSortCarSpeed/Program.cs
@@ -12,11 +12,11 @@ namespace AngleOverloadOperatorSortCarSpeed
         static void Main(string[] args)
         {
 
-            Angle ang1 = new Angle(5,45,23);
-            Angle ang2 = new Angle(-2,52,38);
+            Angle ang1 = new Angle(6,52,45);
+            Angle ang2 = new Angle(5,45,23);
             WriteLine(ang1.ToString());
             WriteLine(ang2.ToString());
-            Angle ang3 = ang1 + ang2;
+            Angle ang3 = ang2 - ang1;
             WriteLine();
             WriteLine(ang3.ToString());
             WriteLine();
